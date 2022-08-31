3634956e24b6b205866a86a4c79eb23b865b2691
Sayed Tahsin <mail.tahsin99@gmail.com>
2022-09-01 02:03:02 +0600

    day3 2:02

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:fullter_tutorial/home_page.dart';
-import 'home_page.dart';
+import 'package:fullter_tutorial/pages/home_page.dart';
+import 'pages/home_page.dart';
+
 void main() {
   runApp(Myapp());
 }
 
 class Myapp extends StatelessWidget {
-
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.red),
+      darkTheme:
+          ThemeData( primarySwatch: Colors.cyan),
     );
   }
 }
