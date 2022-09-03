cc74b5eb71675f45d47615fbb71e941ee62ca3ee
Sayed Tahsin <mail.tahsin99@gmail.com>
2022-09-04 01:51:46 +0600

    4/9/2022 1:51

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-
-  final  int days = 30;
+  final int days = 30;
   final String name = "Tahsin";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter Day2"),
+        title: const Text("Flutter Catalog"),
       ),
       body: Center(
-        child: Container(
-          child: Text("Welcome $days flutter course by $name"),
-        ),
+        child: Text("Welcome $days flutter course by $name"),
       ),
-    drawer: Drawer(),
+      drawer: Drawer(),
     );
   }
-  
 }
