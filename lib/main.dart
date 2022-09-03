3fc0b2ee2fc43f8798bcc1b501cbbba868d8bfd8
Sayed Tahsin <mail.tahsin99@gmail.com>
2022-09-03 12:12:51 +0600

    day 4 completed 3/9/2022 12:12

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fullter_tutorial/pages/home_page.dart';
 import 'package:fullter_tutorial/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(primarySwatch: Colors.red,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(primarySwatch: Colors.cyan),
       initialRoute: "/",
       routes: {
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -13,11 +13,50 @@ class LoginPage extends StatelessWidget {
               "assets/images/login_image.png",
               fit: BoxFit.cover,
             ),
+            SizedBox(
+              height: 30.0,
+            ),
             Text(
               "Login",
               style: TextStyle(
                 fontSize: 30,
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 50),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  ElevatedButton(
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.blue),
+                    onPressed: () {
+                      // ignore: avoid_print
+                      print("Hello piter");
+                    },
+                    child: Text("Login"),
+                  )
+                ],
               ),
             )
           ],
