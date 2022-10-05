0a31e7aec1e7d2516a308b1f7d579c03a52bda4b
Sayed Tahsin <mail.tahsin99@gmail.com>
2022-10-06 01:22:01 +0600

    day 22 complete

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
       theme: MyTheme.LightTheme(context),
       darkTheme: MyTheme.DarkTheme(context),
       debugShowCheckedModeBanner: false,
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,15 +1,74 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:fullter_tutorial/widgets/theme.dart';
 import 'package:velocity_x/velocity_x.dart';
+
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: context.canvasColor,
-        title: "Cart".text.make(),
+        appBar: AppBar(
+          backgroundColor: context.canvasColor,
+          title: "Cart".text.make(),
+        ),
+        body: Column(
+          children: [
+            _CartList().p32().expand(),
+            Divider(),
+            _CartTotal(),
+          ],
+        ));
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  // const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$777".text.xl4.color(context.theme.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(context.theme.backgroundColor),
+            ),
+            child: "Buy".text.white.make(),
+          ).w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  // const _CartList({super.key});
+
+  @override
+  State<_CartList> createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
+        title: "Item1".text.make(),
       ),
     );
   }
